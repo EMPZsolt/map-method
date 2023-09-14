3aa0e27a50ee727fbd6d133893145ab1c699daa3
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2023-09-14 21:24:59 +0000

    Initial commit

--- map.js
+++ map.js
@@ -5,17 +5,33 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
 
+let results = [];
+for (num of nums) {
+  results.push(num*2);
+}
+
+console.log(results);
 
 // Using map()
 
+const multipleByTwo = function (num) {
+  return num*2;
+};
+
+const mapResults = nums.map(multipleByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const simplified = nums.map (function(num) {return num*2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
 
-
+const arrow = nums.map (num => num*2);
+console.log(arrow);
 // With objects:
 const students = [
   {
@@ -37,3 +53,20 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map (student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentObject = students.map (student => ({
+  id: student.id,
+  name: student.name
+}));
+console.log(studentObject);
+
+const studentAges = [28, 30, 26];
+
+const studentsWithAge = students.map ((student, index) => ({
+  ...student,
+  age: studentAges[index]
+}));
+console.log(studentsWithAge);
